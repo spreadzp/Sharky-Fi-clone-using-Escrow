cc8e91b19025e6ff18346eff90ed3e9d325bbf52
Paul K <pavlo.k@blaize.tech>
2023-11-08 23:27:34 +0200

    set address

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("ungaE9gfvL1r3eUWqn1nki684J36n2QVtTwrHMvDcdL");
 
 #[program]
 pub mod nft_lend_borrow {
